7e1b6e59011b0b56b2c7746d0d5bca11655313b2
Brian Butcher <brian@meaningmachine.schoolhouse>
2024-08-29 17:45:07 +0100

    modified:   README 	new file:   config_dev.py

---
+++ config_dev.py
@@ -0,0 +1,32 @@
+# These are specimen configuration values. You should change them all for values appropriate to you.
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config:
+    LP_GROUP_NAME="Brian's Groups"
+    SQLALCHEMY_DATABASE_URI= 'sqlite:///app.db'
+    SQLALCHEMY_ENGINE_OPTIONS= {'pool_recycle' : 280}
+    SECRET_KEY = 'top secret'
+    LP_ADMIN = 'administrator_email@example.com'
+    DEFAULT_GROUP_NAME = 'Phil'
+
+    # Mail Globals appropriate to my philosopy group
+    MAIL_DEFAULT_SENDER='my email address'
+    LP_MAIL_SUBJECT_PREFIX='[Philosophy Group Name]'
+    LP_MAIL_SENDER =' <admistrator@my.philosophy.group>' 
+
+    # Gmail configuration globals
+    MAIL_USERNAME='me@gmail.com'
+    MAIL_PASSWORD='google allocated password'
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USE_SSL = False
+    MAIL_DEBUG = True
+    MAIL_SUPPRESS_SEND = False
+
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    @staticmethod
+    def init_app(app):
+        pass
+
